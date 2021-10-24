8b560413710b23bbbdbc3398a294de2a74a2000f
wac <171261750@qq.com>
2021-10-24 23:14:10 +0800

    用户模块接口 添加用户+查询用户及分页

--- model/user.go
+++ model/user.go
@@ -1,10 +1,42 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"ginblog/utils/errmsg"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"type:varchar(20);not null" json:"username"`
-	Password string `gorm:"type:varchar(20);not null " json:"password"`
-	Role     int    `gorm:"" json:"role"`
+	Username string `gorm:"type:varchar(20);not null" json:"username" binding:"required"`
+	Password string `gorm:"type:varchar(20);not null " json:"password" binding:"required"`
+	Role     int    `gorm:"type:int" json:"role" binding:"required"`
+}
+
+// 查询用户
+func CheckUser(name string) int {
+	var user User
+	db.Select("id").Where("username = ?", name).First(&user)
+	if user.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCESS
+}
+
+// 创建用户
+func CreateUser(user *User) int {
+	err := db.Create(user).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// 查询用户列表
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err := db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return users
 }
