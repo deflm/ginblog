face41ed8bd0b8d467e20c18e98bf6009d7a3d34
wac <171261750@qq.com>
2021-10-25 21:30:53 +0800

    博客文章接口

--- routes/router.go
+++ routes/router.go
@@ -23,7 +23,15 @@ func InitRouter() {
 		router.GET("category/:id", v1.GetCateInfo)
 		router.PUT("category/:id", v1.EditCate)
 		router.DELETE("category/:id", v1.DeleteCate)
+
 		// article
+		router.POST("article/add", v1.AddArticle)
+		router.GET("article", v1.GetArt)
+		router.GET("article/list/:id", v1.GetCateArt)
+		router.GET("article/info/:id", v1.GetArtInfo)
+		router.PUT("article/:id", v1.EditArt)
+		router.DELETE("article/:id", v1.DeleteArt)
+
 	}
 	panic(r.Run(utils.HttpPort))
 }
